6520fbd44757ee2a338d8fe4a91065ee5a50d474
Muhammad Faizan <m.faizan@sap.com>
2021-11-29 23:12:24 +0100

    Bumped reconcile images to bf549c8

--- script.sh
+++ script.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-git checkout -b test_branch
+export BRANCH_NAME=test_branch
+export RECONCILER_IMAGE_TAG=$(git rev-parse --short HEAD)
+export VALUES_YAML_PATH="values.yaml"
 
-yq e '.global.images.mothership_reconciler.tag' values.yaml
+echo ${RECONCILER_IMAGE_TAG}
 
-yq e -i '.global.images.mothership_reconciler.tag = "cool123456"' values.yaml
+git checkout -B ${BRANCH_NAME}
 
-git commit -m "dasda"
+# yq e '.global.images.mothership_reconciler.tag' values.yaml
 
-git push --set-upstream origin test_branch
+yq e -i '.global.images.mothership_reconciler.tag = "'"${RECONCILER_IMAGE_TAG}"'"' ${VALUES_YAML_PATH}
+yq e -i '.global.images.component_reconciler.tag = "'"${RECONCILER_IMAGE_TAG}"'"' ${VALUES_YAML_PATH}
 
-gh pr create --base main --title "My first cli PR" --body "What did I do?"
+git commit -a -m "Bumped reconcile images to ${RECONCILER_IMAGE_TAG}"
+
+git push --set-upstream origin ${BRANCH_NAME}
+
+# gh pr status --json state | jq '.currentBranch."state"'
+
+# gh pr create --base main --title "My first cli PR" --body "What did I do?"
 
 
-git rev-parse --short HEAD
+# git rev-parse --short HEAD
 
 
-gh pr status --json state | jq '.currentBranch."state"'
-"MERGED"
+
+#"MERGED"
